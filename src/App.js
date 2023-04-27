94ef66cca6514951f324fb8ac6e27325f8518527
manav9556 <54392955+manav9556@users.noreply.github.com>
2023-04-27 13:57:50 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Chat from './components/Chat/Chat';
+import Join from './components/Join/Join';
+
+const App = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<Join />} />
+      <Route path="/Chat" element={<Chat/>} />
+    </Routes>
+  </Router>
+);
+
+export default App;
---
+++ src/components/Chat/Chat.js
@@ -0,0 +1,83 @@
+import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import queryString from "query-string";
+import TextContainer from "../TextContainer/TextContainer";
+import io from "socket.io-client";
+import "./Chat.css";
+import "../Infobar/InfoBar.css";
+import Input from "../Input/Input";
+import InfoBar from "../Infobar/InfoBar";
+import Messages from "../Messages/Messages";
+
+let socket;
+
+const Chat = () => {
+  const location = useLocation();
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+  const [users, setUsers] = useState([]);
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+  const ENDPOINT = "http://localhost:5000";
+
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
+
+    socket = io(ENDPOINT);
+
+    setRoom(room);
+    setName(name);
+
+    socket.emit("join", { name, room }, (error) => {
+      if (error) {
+        alert(error);
+      }
+    });
+
+    return () => {
+      socket.emit("disconnect");
+      socket.off();
+    };
+  }, [ENDPOINT, location.search]);
+
+  useEffect(() => {
+    socket.on("message", (message) => {
+      setMessages((messages) => [...messages, message]);
+    });
+
+    socket.on("roomData", ({ users }) => {
+      setUsers(users);
+    });
+  }, []);
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+
+    if (message) {
+      socket.emit("sendMessage", message, (error) => {
+        if (error) {
+          alert(error);
+        } else {
+          setMessage("");
+        }
+      });
+    }
+  };
+
+  return (
+    <div className="outerContainer">
+      <div className="container">
+        <InfoBar room={room} />
+        <Messages messages={messages} name={name} />
+        <Input
+          message={message}
+          setMessage={setMessage}
+          sendMessage={sendMessage}
+        />
+      </div>
+      <TextContainer users={users} />
+    </div>
+  );
+};
+
+export default Chat;
